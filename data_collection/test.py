a62e9f7c7e3e664154e25a5974b4123051e23564
ilmal <nils@u1.se>
2022-05-21 15:37:28 +0200

    update remote

---
+++ data_collection/test.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import json
+import time
+
+
+def main():
+
+    #df = pd.read_csv("./data_points/AAPL.csvvv")
+
+    #df.reset_index(inplace=True, drop=True)
+
+    # print(df.head(5))
+
+    #df = df.drop(columns=["Unnamed: 0", "Unnamed: 0.1"])
+
+    # print(df.head(5))
+
+    # df.to_csv("./data_points/AAPL.csvvv")
+
+
+if __name__ == "__main__":
+
+    main()
