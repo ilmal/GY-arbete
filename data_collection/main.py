2447d561321a5889575c95962ab7c176b0461825
ilmal <nils@u1.se>
2022-05-21 15:35:37 +0200

    update to remote

--- data_collection/main.py
+++ data_collection/main.py
@@ -1,13 +1,67 @@
-import csv
-import requests
+import pandas as pd
 
-# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-CSV_URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=15min&slice=year1month1&apikey=demo'
+#from dataGrab import grab_data_func
+from reshapeData import reshape_data_func, split_data_to_days_func
+from misc import get_raw_data
 
-with requests.Session() as s:
-    download = s.get(CSV_URL)
-    decoded_content = download.content.decode('utf-8')
-    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
-    my_list = list(cr)
-    for row in my_list:
-        print(row)
+
+def get_data_from_company(data_slices, company):
+    raw_data = get_raw_data(
+        data_slices, company)
+
+    raw_data_arr = split_data_to_days_func(raw_data)
+
+    data_arr = []
+    for raw_data in raw_data_arr:
+        data_arr.append(reshape_data_func(raw_data))
+
+    df = pd.concat(data_arr)
+
+    return df
+
+
+def main():
+    """
+
+   1. grab data with get_raw_data func.
+
+   2. format the data with reshape func
+
+
+
+    """
+
+    companies = ["TSLA", "AAPL", "GOOGL", "MSFT", "AMZN",
+                 "JNJ", "NVDA", "FB", "UNH", "XOM"]
+
+    companies = ["GOOGL", "MSFT", "AMZN",
+                 "JNJ", "NVDA", "FB", "UNH", "XOM"]
+
+    data_slices = 24  # from 0 - 24
+
+    ###############################
+
+    # CURRENT PROBLEM IS THAT get_data_from_company returns array, and so data array is an array of arrays, which I cannot concat into pandas obj
+
+    ###############################
+
+    data_arr = []
+    for company in companies:
+        data = get_data_from_company(data_slices, company)
+        data_arr.append(data)
+
+        data.reset_index(inplace=True, drop=True)
+
+        print(f"writing data to {company}.csv (/data_points/)")
+        data.to_csv(f"./data_points/{company}.csv")
+
+    df = pd.concat(data_arr)  # combine all data to a single dataframe
+
+    # reset the index of the dataframe and update dataframe
+    df.reset_index(drop=True, inplace=True)
+
+    df.to_csv("out.csv")
+
+
+if __name__ == "__main__":
+    main()
